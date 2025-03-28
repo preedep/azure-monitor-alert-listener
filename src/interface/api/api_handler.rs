a4110b28fe0370db4799d3e8255a4eebbb618013
Preedee Ponchevin <preedee.digital@gmail.com>
2025-03-28 21:22:08 +0700

    Add email sending functionality to alert handler
    Integrated email sending via `send_email_with_api` in the alert handler, using app state to fetch authentication and configuration details. Updated the function signature to include `AppState` and added proper logging for success and error scenarios when sending emails.

--- src/interface/api/api_handler.rs
+++ src/interface/api/api_handler.rs
@@ -2,23 +2,44 @@ use crate::domain::models::AzureMonitorAlert;
 use actix_web::{post, web, HttpResponse, Responder};
 use log::{debug, error};
 use serde_json::Value;
+use crate::application::mail::mail_sender::send_email_with_api;
 use crate::application::mail::template_render::render_alert_email;
+use crate::interface::state::AppState;
 
 #[post("/alert")]
-pub async fn receive_alert(payload: web::Json<Value>) -> impl Responder {
+pub async fn receive_alert(app_state: web::Data<AppState>,payload: web::Json<Value>) -> impl Responder {
     debug!(
         "📦 Raw JSON Payload:\n{}",
         serde_json::to_string_pretty(&payload).unwrap()
     );
     match AzureMonitorAlert::try_from(payload) {
         Ok(alert) => {
+            let state = app_state.get_ref();
             //debug!("✅ Got alert: {:?}", alert.data.essentials.alert_rule);
             debug!("Alert payload: {:#?}", alert);
             let ret = render_alert_email("template/*", "mail_template.html", &alert);
             match ret {
                 Ok(html) => {
                     debug!("📧 Email HTML:\n{}", html);
-
+                    let res = send_email_with_api(
+                        &state.tenant_id,
+                        &state.client_id,
+                        &state.client_secret,
+                        &state.asc_url,
+                        &state.sender,
+                        &state.reply_to,
+                        &state.display_name,
+                        &alert.data.essentials.alert_rule,
+                        &html,
+                    ).await;
+                    match res {
+                        Ok(_) => {
+                            debug!("✉️ Email sent successfully");
+                        }
+                        Err(e) => {
+                            error!("❌ Error sending email: {}", e);
+                        }
+                    }
                 }
                 Err(e) => {
                     error!("❌ Error rendering email: {}", e);
