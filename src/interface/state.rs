36726fc2b1dc4a007b5915f9d04db48facc596f3
Preedee Ponchevin <preedee.digital@gmail.com>
2025-03-28 21:15:28 +0700

    Add AppState for managing configuration across handlers
    Introduced the `AppState` struct to centralize application configuration. Updated the HTTP server to share this state with handlers using Actix's data mechanism. This ensures cleaner and more maintainable code for managing shared environment variables.

---
+++ src/interface/state.rs
@@ -0,0 +1,12 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug,Clone, Serialize, Deserialize)]
+pub struct AppState {
+    pub(crate) tenant_id: String,
+    pub(crate) client_id: String,
+    pub(crate) client_secret: String,
+    pub(crate) asc_url: String,
+    pub(crate) sender: String,
+    pub(crate) reply_to: String,
+    pub(crate) display_name: String,
+}
--- src/main.rs
+++ src/main.rs
@@ -13,9 +13,29 @@ async fn main() -> std::io::Result<()> {
     let port = std::env::var("PORT").unwrap_or("8080".to_string());
     info!("Starting Azure Monitor Alert Listener on port {}", port);
 
+    let tenant_id = std::env::var("TENANT_ID").expect("TENANT_ID must be set");
+    let client_id = std::env::var("CLIENT_ID").expect("CLIENT_ID must be set");
+    let client_secret = std::env::var("CLIENT_SECRET").expect("CLIENT_SECRET must be set");
+    let asc_url = std::env::var("ASC_URL").expect("ASC_URL must be set");
+    let sender = std::env::var("SENDER").expect("SENDER must be set");
+    let reply_to = std::env::var("REPLY_EMAIL").expect("REPLY_TO must be set");
+    let display_name = std::env::var("REPLY_EMAIL_DISPLAY").expect("DISPLAY_NAME must be set");
+
+    let state = interface::state::AppState {
+        tenant_id,
+        client_id,
+        client_secret,
+        asc_url,
+        sender,
+        reply_to,
+        display_name,
+    };
+
     let port: u16 = port.parse().unwrap();
-    HttpServer::new(|| {
+    HttpServer::new(move || {
         actix_web::App::new()
+            .app_data(
+                actix_web::web::Data::new(state.clone()))
             .wrap(actix_web::middleware::Logger::default())
             .service(interface::api::api_handler::receive_alert)
     })
