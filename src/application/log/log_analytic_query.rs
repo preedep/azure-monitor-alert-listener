4eca5011aa72d0d3e0afa30a092bec7ec1642a6e
Preedee Ponchevin <preedee.digital@gmail.com>
2025-03-29 15:54:28 +0700

    Add query_link function to handle Azure API log analytics
    Introduced a new `query_link` function to process log analytics queries using Azure API. Updated `api_handler.rs` to include conditional logic for parsing and handling log query links. Adjusted dependencies in `Cargo.toml` to support Azure authentication and HTTP requests.

---
+++ src/application/log/log_analytic_query.rs
@@ -0,0 +1,7 @@
+
+pub async fn query_link(tenant_id: &str,
+                        client_id :&str ,
+                        client_secret:&str,
+                        log_link:&str) {
+
+}
--- src/interface/api/api_handler.rs
+++ src/interface/api/api_handler.rs
@@ -2,6 +2,7 @@ use crate::domain::models::AzureMonitorAlert;
 use actix_web::{post, web, HttpResponse, Responder};
 use log::{debug, error};
 use serde_json::Value;
+use crate::application::log::log_analytic_query::query_link;
 use crate::application::mail::mail_sender::send_email_with_api;
 use crate::application::mail::template_render::render_alert_email;
 use crate::interface::state::AppState;
@@ -15,6 +16,25 @@ pub async fn receive_alert(app_state: web::Data<AppState>,payload: web::Json<Val
     match AzureMonitorAlert::try_from(payload) {
         Ok(alert) => {
             let state = app_state.get_ref();
+            if let Some(condition) = &alert.data.alert_context {
+                if let Some(all_of) = &condition.condition.all_of {
+                    if let Some(first_condition) = all_of.get(0) {
+                        debug!("Condition: {:#?}", first_condition);
+                        if let Some(link_api) = &first_condition.link_to_filtered_search_results_api {
+                            debug!("Link to API: {}", link_api);
+                            query_link(&state.tenant_id,
+                                        &state.client_id,
+                                        &state.client_secret,
+                                        link_api).await;
+                        }
+                    }
+                }
+                debug!("Condition: {:#?}", condition);
+            } else {
+                debug!("No condition found in alert context");
+            }
+
+
             //debug!("✅ Got alert: {:?}", alert.data.essentials.alert_rule);
             debug!("Alert payload: {:#?}", alert);
             let ret = render_alert_email("template/*", "mail_template.html", &alert);
